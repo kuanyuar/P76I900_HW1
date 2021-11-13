033ddf28268e5ea711f3535bfcfdec9cbe30d1f1
kuanyuar <kuanyuar@gmail.com>
2021-11-14 05:54:14 +0800

    Add files via upload

--- hw3_CBOW.py
+++ hw3_CBOW.py
@@ -1,9 +1,6 @@
 import os,sys
 import nltk
 import re
-import math
-import numpy as np
-import csv
 
 nltk.download('stopwords')
 import matplotlib.pyplot as plt
@@ -15,8 +12,8 @@ from gensim.models import word2vec
 
 #folder_path = QFileDialog.getExistingDirectory(self,"Open folder","./")
 #folder_path="./hw2_data"
-folder_path="./constact"
-#folder_path="./test"
+#folder_path="./constact"
+folder_path="./test"
 files= os.listdir(folder_path) #得到資料夾下的所有檔名稱
 
 top = int(input("請輸入欲顯示詞頻Top數:"))
@@ -27,8 +24,8 @@ stopword = stopwords.words('english')
 STR_nostopwords=[]
 TOL_STR=[]
 # Settings
-seed = 6 #亂數種子
-sg = 1 #演算法，預設為0，代表是CBOW，若設為1則是使用Skip-Gram
+#seed = 6 #亂數種子
+sg = 0 #演算法，預設為0，代表是CBOW，若設為1則是使用Skip-Gram
 window_size = 10 #周圍詞彙要看多少範圍
 vector_size = 30 #轉成向量的維度
 min_count = 3 #該詞最少出現幾次，才可以被當作是訓練資料
@@ -73,15 +70,15 @@ model = word2vec.Word2Vec(
                                     epochs=epochs,
                                     window=window_size,
                                     sg=sg,
-                                    seed=seed,
+                                    #seed=seed,
                                     batch_words=batch_words
                                 )
-print('stopword:'+str(len(stopword)))
-print(stopword)
+#print('stopword:'+str(len(stopword)))
+#print(stopword)
 model.save('word2vec.model')
 models = word2vec.Word2Vec.load('word2vec.model')
 #print(models.wv['endometriosis'].shape)
 #print(models.wv.most_similar(key))
-for item in models.wv.similar_by_word(key, topn =top):
+for item in models.wv.similar_by_word(key, topn =top-1):
     print(item)
 
